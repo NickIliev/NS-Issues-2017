59357c9b57b56fe29aad5df608a356ef3eebd77d
NickIliev <Nikolay.Iliev@telerik.com>
2017-01-24 10:58:35 +0200

    add

--- stackoverflow/getChilder/app/main-page.ts
+++ stackoverflow/getChilder/app/main-page.ts
@@ -11,19 +11,19 @@ export function navigatingTo(args: EventData) {
     let stack = <StackLayout>page.getViewById("st");
     let label = <Label>page.getViewById("lbl-title");
 
-    console.log("page.content: " + page.content);
+    console.log("page.content: " + page.content); // page.content: StackLayout<st>@file:///app/main-page.xml:19:5;
 
-    console.log("stack.getChildrenCount(): " + stack.getChildrenCount())
-    console.log("stack.getChildAt(1): " + stack.getChildAt(1));
-    console.log("stack.getChildIndex(label): " + stack.getChildIndex(label));
+    console.log("stack.getChildrenCount(): " + stack.getChildrenCount()) // 3
+    console.log("stack.getChildAt(1): " + stack.getChildAt(1)); // stack.getChildAt(1): Button(5)@file:///app/main-page.xml:21:9;
+    console.log("stack.getChildIndex(label): " + stack.getChildIndex(label)); // 0
 
-    console.log("stack.parent: " + stack.parent);
+    console.log("stack.parent: " + stack.parent); // stack.parent: Page(1)@file:///app/main-page.xml:7:1;
 
     var newlabel = new Label();
     newlabel.text = "new Label";
     stack.addChild(newlabel);
 
-    stack.insertChild(newlabel, 2);
+
 
     page.bindingContext = new HelloWorldModel();
 }
