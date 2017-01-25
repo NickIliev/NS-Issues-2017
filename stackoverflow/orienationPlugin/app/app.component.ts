6d40165a8d93beb7216c15d313c0d0129e601343
NickIliev <Nikolay.Iliev@telerik.com>
2017-01-25 12:47:59 +0200

    addd

--- stackoverflow/orienationPlugin/app/app.component.ts
+++ stackoverflow/orienationPlugin/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component} from "@angular/core";
 var orientation = require('nativescript-orientation');
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent implements OnInit {
-    ngOnInit() {
-        orientation.enableRotation();
-    }
+export class AppComponent {
 
-  
     public onTap() {
         console.log(orientation.getOrientation());  
     }
