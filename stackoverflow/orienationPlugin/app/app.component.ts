ee3a4c8d77310a18a1a471217f987441c222b221
NickIliev <Nikolay.Iliev@telerik.com>
2017-01-25 12:41:28 +0200

    add

--- stackoverflow/orienationPlugin/app/app.component.ts
+++ stackoverflow/orienationPlugin/app/app.component.ts
@@ -6,30 +6,12 @@ var orientation = require('nativescript-orientation');
     templateUrl: "app.component.html",
 })
 export class AppComponent implements OnInit {
-    public counter: number = 16;
-
     ngOnInit() {
         orientation.enableRotation();
     }
 
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-    
+  
     public onTap() {
-        this.counter--;
         console.log(orientation.getOrientation());  
-    }
-
-    public setLandscapeOrientation() {
-        orientation.setOrientation("landscape");  
-    }
-
-    public setPortraitOrientation() {
-        orientation.setOrientation("portrait");  
     }
 }
