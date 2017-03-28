936a7fd5bc0707223ab50411dd4ea6b92a46c218
NickIliev <Nikolay.Iliev@telerik.com>
2017-03-28 11:33:05 +0300

    add

--- NativeScript/issue_3873/app/main-page.ts
+++ NativeScript/issue_3873/app/main-page.ts
@@ -12,14 +12,14 @@ let vm = new ViewModel();
 var lv: ListView;
 
 export function selectItemTemplate(item: Item, index: number, items: ObservableArray<Item>): string {
-    return item.id % 10 === 0 ? "red" : item.id % 2 === 0 ? "green" : "yellow"; 
+    return item.id % 10 === 0 ? "red" : item.id % 2 === 0 ? "green" : "yellow";
 }
 
 export function pageLoaded(args: EventData) {
-  let page = <Page>args.object;
-  lv = <ListView>page.getViewById("lv2");
+    let page = <Page>args.object;
+    lv = <ListView>page.getViewById("lv2");
 
-  page.bindingContext = vm;
+    page.bindingContext = vm;
 }
 
 
@@ -45,13 +45,21 @@ export function onItemTap(args: EventData) {
     // vm.set("items", new ObservableArray([new Item("new item", 111), new Item("new item", 222), new Item("new item", 333), new Item("new item", 444), new Item("new item", 555), new Item("new item", 666), new Item("new item", 777), new Item("new item", 888)]))
 }
 
+
+export function onStackTap(args: EventData) {
+    console.log("onStackTap fired");
+} 
+export function onTap(args: EventData) {
+    console.log("onTap fired");
+} 
+
 let scrollToBottom = true;
-export function onScroll(args: EventData){
-  let page = (<View>args.object).page;
-  let gridLayout = page.getViewById<GridLayout>("grid-layout");
-  for (let i = 0, length = gridLayout.getChildrenCount(); i < length; i++){
-      let listView = <ListView>gridLayout.getChildAt(i);
-      listView.scrollToIndex(scrollToBottom ? listView.items.length - 1 : 0);
-  }
-  scrollToBottom = !scrollToBottom;
+export function onScroll(args: EventData) {
+    let page = (<View>args.object).page;
+    let gridLayout = page.getViewById<GridLayout>("grid-layout");
+    for (let i = 0, length = gridLayout.getChildrenCount(); i < length; i++) {
+        let listView = <ListView>gridLayout.getChildAt(i);
+        listView.scrollToIndex(scrollToBottom ? listView.items.length - 1 : 0);
+    }
+    scrollToBottom = !scrollToBottom;
 }
