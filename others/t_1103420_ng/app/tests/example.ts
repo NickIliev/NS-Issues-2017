a8ddfdc2cda09ebbec1463d91ef1f72dae7ffd93
Steve Jackson <stevenljackson1@gmail.com>
2017-04-12 17:35:03 -0400

    Updates to sample code
    Use karma-browserify
    Add in shim for reflect-metadata
    Still getting "Uncaught Error: Cannot find module '@angular/core'"

--- others/t_1103420_ng/app/tests/example.ts
+++ others/t_1103420_ng/app/tests/example.ts
@@ -1,9 +1,24 @@
 import * as reflect from "reflect-metadata";
- 
+import {AppComponent} from "../app.component";
+
 // A sample Jasmine test
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
+  });
+
+  it("should define a click count", function() {
+    let appComponent = new AppComponent();
+
+    expect(appComponent.counter).toBe(16);
+  });
+
+  it("should report clicks accurately", function() {
+    let appComponent = new AppComponent();
+
+    appComponent.onTap();
+    expect(appComponent.counter).toBe(15);
+    expect(appComponent.message).toBe("15 taps left");
   });
 });
 
