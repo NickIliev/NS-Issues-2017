533606688047a09997a58890e5a5ffe1c4c8bd56
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-04-24 10:33:57 +0300

    add

---
+++ NativeScript/issue_4052/app/sub-page.js
@@ -0,0 +1,7 @@
+"use strict";
+var frameModule = require("ui/frame");
+function goBack() {
+    frameModule.goBack();
+}
+exports.goBack = goBack;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3ViLXBhZ2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJzdWItcGFnZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsc0NBQXdDO0FBRXhDO0lBQ0ksV0FBVyxDQUFDLE1BQU0sRUFBRSxDQUFDO0FBQ3pCLENBQUM7QUFGRCx3QkFFQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIGZyYW1lTW9kdWxlIGZyb20gXCJ1aS9mcmFtZVwiO1xuXG5leHBvcnQgZnVuY3Rpb24gZ29CYWNrKCkge1xuICAgIGZyYW1lTW9kdWxlLmdvQmFjaygpO1xufSJdfQ==
---
+++ NativeScript/issue_4052/app/sub-page.ts
@@ -0,0 +1,5 @@
+import * as frameModule from "ui/frame";
+
+export function goBack() {
+    frameModule.goBack();
+}
