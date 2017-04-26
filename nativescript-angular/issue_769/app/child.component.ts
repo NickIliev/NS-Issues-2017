993782676841b9dbe8314a8829e39472c7754df3
Nick Iliev <nikolay.iliev@telerik.com>
2017-04-26 13:26:56 +0300

    add

--- nativescript-angular/issue_769/app/child.component.ts
+++ nativescript-angular/issue_769/app/child.component.ts
@@ -4,17 +4,12 @@ import { Component } from "@angular/core";
 	selector:'child',
 	moduleId: module.id,
 	template: `
-	<StackLayout>
+	<StackLayout class="child-class">
 		<Label text="Label 1" class="label-one"></Label>
 		<Label text="Label 2" class="label-two"></Label>
 	</StackLayout>
 	`,
 	styles: [`
-    StackLayout {
-        border-width : 2;
-        border-color: red;
-        height: 400;
-    }
 	.label-one {
 		color: red;
 	}
