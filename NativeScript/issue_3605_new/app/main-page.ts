6dda32bb81cebbb93cf2c96d2d9ce724d9ac0825
Nikolay Iliev <niliev@progress.com>
2017-04-26 11:02:10 +0300

    add

--- NativeScript/issue_3605_new/app/main-page.ts
+++ NativeScript/issue_3605_new/app/main-page.ts
@@ -13,18 +13,23 @@ export function navigatingTo(args: EventData) {
     var folderPath = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOWNLOADS).toString();
     console.log("folderPath; " + folderPath)
 
-    // var documents = fs.knownFolders.documents();
-    // console.log("newFolderPath: " + documents);
+    var documents = fs.knownFolders.documents();
+    console.log("documents: " + documents);
 
     var fileName = 'img_' + new Date().getTime() + '.png';
     console.log("fileName: " + fileName);
 
-    var path = fs.path.join(folderPath, fileName);
+    var path = fs.path.join(documents.path, fileName);
     console.log("path: " + path);
 
     var img = imageSourceModule.fromResource("icon");
     console.log("img: " + img);
 
-    var saved = img.saveToFile(path, enums.ImageFormat.png);
-    console.log(saved)
+    var exists = fs.File.exists(path);
+    console.log(exists);
+
+    if (!exists) {
+        var saved = img.saveToFile(path, enums.ImageFormat.png);
+        console.log(saved);
+    }
 }
