efe4bdbbfa751643c2fde092c5a35e7db949e678
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-05-01 16:23:01 +0300

    add

--- NativeScript/issue_4079/app/main-page.ts
+++ NativeScript/issue_4079/app/main-page.ts
@@ -2,8 +2,17 @@ import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
 import { topmost } from "ui/frame";
 
-export function navigatingTo(args: EventData) {
+declare var UIColor: any;
+
+export function onLoaded(args: EventData) {
     let page = <Page>args.object;
+
+    topmost().ios.navBarVisibility = "always";
+
+    var controller = topmost().ios.controller;
+
+    controller.navigationBar.tintColor = UIColor.redColor;
+    controller.navigationBarHIdden = false;
 }
 
 export function onTap(args: EventData) {
