ea9a130e5a757a749335d127ae6416a85a814f45
Nick Iliev <nikolay.iliev@telerik.com>
2017-05-03 10:21:17 +0300

    add

--- NativeScript/issue_4091/app/main-page.ts
+++ NativeScript/issue_4091/app/main-page.ts
@@ -9,18 +9,7 @@ export function navigatingTo(args: EventData) {
     page.bindingContext = viewModel;
 }
 
-export function onTapWithoutTimeout() {
-
-    viewModel.set('busy', true);
-
-    for (var i = 0; i < 10000; i++) {
-        console.log("step " + i);
-    }
-    viewModel.set('busy', false);
-
-}
-
-export function onTapWithTimeout() {
+export function onTap() {
 
     viewModel.set('busy', !viewModel.get("busy"));
 
