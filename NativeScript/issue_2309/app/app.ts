ae0adc46a72b03cfa3593b45714850c2822bdb39
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-05-18 10:00:26 +0300

    add

--- NativeScript/issue_2309/app/app.ts
+++ NativeScript/issue_2309/app/app.ts
@@ -1,16 +1,16 @@
 ﻿"use strict";
-import { on, start, uncaughtErrorEvent } from "application";
+import { on, start, uncaughtErrorEvent, android, ios } from "application";
 
 on(uncaughtErrorEvent, (args) => {
-    if (args.android) {
+    if (android) {
         // For Android applications, args.android is an NativeScriptError.
         console.log(" *** NativeScriptError *** : " + args.android);
         console.log(" *** StackTrace *** : " + args.android.stackTrace);
         console.log(" *** nativeException *** : " + args.android.nativeException);
-    }
-    else if (args.ios) {
+
+    } else if (ios) {
         // For iOS applications, args.ios is NativeScriptError.
-        console.log("NativeScriptError: " + args.ios);
+        console.log(" ||||| NativeScriptError in iOS ||||| " + args.ios);
     }
 });
 start({ moduleName: "main-page" });
