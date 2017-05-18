292b349e259c6d83fad7aa730a8d0e7a44aeac7b
Nick Iliev <nikolay.iliev@telerik.com>
2017-05-18 09:42:52 +0300

    add

---
+++ NativeScript/issue_2309/app/app.ts
@@ -0,0 +1,16 @@
+﻿"use strict";
+import { on, start, uncaughtErrorEvent } from "application";
+
+on(uncaughtErrorEvent, (args) => {
+    if (args.android) {
+        // For Android applications, args.android is an NativeScriptError.
+        console.log(" *** NativeScriptError *** : " + args.android);
+        console.log(" *** StackTrace *** : " + args.android.stackTrace);
+        console.log(" *** nativeException *** : " + args.android.nativeException);
+    }
+    else if (args.ios) {
+        // For iOS applications, args.ios is NativeScriptError.
+        console.log("NativeScriptError: " + args.ios);
+    }
+});
+start({ moduleName: "main-page" });
---
+++ NativeScript/issue_2309/app/main-page.ts
@@ -0,0 +1,8 @@
+import { EventData } from 'data/observable';
+import { Page } from 'ui/page';
+import { HelloWorldModel } from './main-view-model';
+
+export function navigatingTo(args: EventData) {
+
+    var btn = new android.widget.Button(null);
+}
