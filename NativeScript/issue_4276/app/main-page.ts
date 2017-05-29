7aaf2d7b8f2c9cd4e6a0917f60d7a4d4b88792e3
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-05-29 11:51:48 +0300

    add

--- NativeScript/issue_4276/app/main-page.ts
+++ NativeScript/issue_4276/app/main-page.ts
@@ -6,9 +6,8 @@ import { Color } from "color";
 
 export function onPageLoaded(args: EventData) {
     let page = <Page>args.object;
+
     let view = <GridLayout>page.getViewById("view1");
-
-
     view.backgroundColor = new Color("red");
-    view.animate({ backgroundColor: new Color("green"), duration: 2000 });
+    view.animate({ backgroundColor: new Color("green"), duration: 2000, iterations: Number.POSITIVE_INFINITY });
 }
