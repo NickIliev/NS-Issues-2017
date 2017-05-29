07ef6316e266d0176b06864fc7cd9db5e4bc2537
Nick Iliev <nikolay.iliev@telerik.com>
2017-05-29 17:16:47 +0300

    add

--- nativescript-angular/issue_826/app/app.module.ts
+++ nativescript-angular/issue_826/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { NativeScriptFormsModule } from "nativescript-angular/forms"
+
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
@@ -13,6 +16,7 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
+        NativeScriptFormsModule,
         AppRoutingModule
     ],
     declarations: [
