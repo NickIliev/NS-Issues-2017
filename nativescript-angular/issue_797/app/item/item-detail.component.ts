77fc43ca5ee76123c4fdbae144c73091c8aa87ae
Nick Iliev <nikolay.iliev@telerik.com>
2017-05-31 16:35:22 +0300

    add

---
+++ nativescript-angular/issue_797/app/item/item-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { RouterExtensions } from "nativescript-angular/router";
+
+import { Item } from "./item";
+import { ItemService } from "./item.service";
+
+import "rxjs/add/operator/map";
+
+@Component({
+    selector: "ns-details",
+    moduleId: module.id,
+    templateUrl: "./item-detail.component.html",
+})
+export class ItemDetailComponent implements OnInit {
+    item: Item;
+    id: number = 5;
+    constructor(
+        private itemService: ItemService,
+        private route: ActivatedRoute,
+        private routerExtensions: RouterExtensions,
+        private nav: Router 
+    ) { }
+
+    ngOnInit() {
+        this.route.params
+            .map(params => this.itemService.getItem(+params["id"]))
+            .subscribe(item => this.item = item);
+    }
+
+    openDetails(): void {
+        console.log("openDetails clicked");
+        console.log(this.id);
+        
+        this.routerExtensions.navigate(['/item', this.id]);  // This doesn't work
+
+        this.id--;
+    }
+
+    goBack() {
+        this.routerExtensions.back();
+    }
+}
---
+++ nativescript-angular/issue_797/readme.md
@@ -0,0 +1,13 @@
+## Install
+
+`$ git clone git@github.com:davorpeic/tns-angular-router-test.git`
+
+`npm install`
+
+`tns run ios`
+
+## Problem
+
+Click on item in the list and and then try opening other details by two differente ways.
+
+![alt text](screenshot.jpg "screenshot")
