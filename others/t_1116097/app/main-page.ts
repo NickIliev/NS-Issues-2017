717e00b96706c31ee9f037c6928177d8718258be
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-09-15 09:47:14 +0300

    add

--- others/t_1116097/app/main-page.ts
+++ others/t_1116097/app/main-page.ts
@@ -8,26 +8,20 @@ import { HelloWorldModel } from './main-view-model';
 import { RadListView, ListViewLinearLayout, ListViewScrollDirection } from "nativescript-telerik-ui-pro/listview"
 
 export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    let page = <Page>args.object;
 
+    let page = <Page>args.object;
 
     let container = <StackLayout>page.getViewById("container");
 
     let radList = new RadListView();
-    radList.items = [1, 2, 3, 4, 5, 6.6];
-    radList.selectionBehavior = "Press";
-    radList.itemTemplate = "<label text='{{ $value }}' />";
+    radList.items = [1, 2, 3, 4, 5, 6.6, 888];
+    radList.itemTemplate = "<StackLayout><Label text='{{ $value }}' /></StackLayout>";
 
     let linearLayout = new ListViewLinearLayout();
-    linearLayout.scrollDirection = "Horizontal";
+    linearLayout.scrollDirection = "Vertical";
     radList.listViewLayout = linearLayout;
 
     container.addChild(radList);
 
-    page.bindingContext = new HelloWorldModel();
+    // page.bindingContext = new HelloWorldModel();
 }
