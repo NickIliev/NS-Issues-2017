e70c1e8f432ce275092a0291ebbdad361c522d3b
Nick Iliev <nikolay.iliev@telerik.com>
2017-09-25 11:38:05 +0300

    add'

---
+++ nativescript-angular/issue_999/app/item/item.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class ItemService {
+    private items = new Array<any>(
+        {
+            "NodeId": 2259,
+            "Number": 1,
+            "Details": "",
+            "Url": ""
+        },
+        {
+            "NodeId": 7534,
+            "Number": 2,
+            "Details": "",
+            "Url": ""
+        },
+        {
+            "NodeId": 3585,
+            "Number": 3,
+            "Details": "",
+            "Url": "http://www.ncbi.nlm.nih.gov/pubmed/18489970"
+        },
+        {
+            "NodeId": 7535,
+            "Number": 4,
+            "Details": "",
+            "Url": "http://www.ncbi.nlm.nih.gov/pubmed/9701682"
+        }
+
+    );
+
+    getItems(): [] {
+        return this.items;
+    }
+}
