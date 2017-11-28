fd54871f1084a13c6cdf4779f4f63c96908963d3
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-11-28 10:39:04 +0200

    add

---
+++ NativeScript/issue_5098/app/main-page.ts
@@ -0,0 +1,56 @@
+import * as camera from 'nativescript-camera';
+import * as fs from 'file-system';
+import { ImageSource, fromAsset } from 'image-source';
+import { ImageAsset } from "image-asset";
+
+import * as bghttp from 'nativescript-background-http';
+let session = bghttp.session('image-upload');
+
+export function onLoaded() {
+    camera.requestPermissions();
+}
+
+
+export function takePhoto() {
+    camera.takePicture()
+        .then((imageAsset: ImageAsset) => {
+            let hash = (new Date()).getTime() + Math.floor(Math.random() * 20);
+            let folder = fs.knownFolders.documents();
+            let path = fs.path.join(folder.path, `Test-${hash}.png`);
+
+            fromAsset(imageAsset)
+                .then((res: ImageSource) => {
+                    let isSaved = res.saveToFile(path, 'png');
+
+                    return isSaved;
+                }).then(isSaved => {
+                    console.log(isSaved); // now that the file is saved we can proceed with the background task
+
+                    if (isSaved) {
+                        let request = {
+                            url: "http://httpbin.org/post",
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/octet-stream",
+                                "File-Name": 'randomName'
+                            },
+                            description: "{ 'uploading': " + 'randomName' + " }"
+                        };
+    
+                        var task = session.uploadFile(path, request);
+    
+                        task.on("progress", logEvent);
+                        task.on("error", logEvent);
+                        task.on("complete", logEvent);
+                    }
+                })
+        }).catch((err) => {
+            console.log(`Error -> ${err.message}`);
+        });
+}
+
+function logEvent(e) {
+    console.log("currentBytes: " + e.currentBytes);
+    console.log("totalBytes: " + e.totalBytes);
+    console.log("eventName: " + e.eventName);
+}
