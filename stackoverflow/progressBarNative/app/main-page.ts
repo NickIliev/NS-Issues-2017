d49bd5cf6ed20cba29418863271e9d31b8e16178
Nick Iliev <nikolay.iliev@telerik.com>
2017-12-07 15:47:54 +0200

    add

--- stackoverflow/progressBarNative/app/main-page.ts
+++ stackoverflow/progressBarNative/app/main-page.ts
@@ -17,8 +17,10 @@ export function onPbLoaded(args) {
         let nativeProgress = pb.android; // android.widget.ProgressBar{d26eea9 V.ED..... ......ID 0,0-0,0}
         console.log("nativeProgress Android: " + nativeProgress); 
 
-        pb.scaleX = 1.5; 
-        pb.scaleY = 4;
+        nativeProgress.scaleX = 1.5; 
+        nativeProgress.scaleY = 4;
+
+        // nativeProgress.setScaleY(5)
         // SCALE_Y added in API level 14
         // Property<View, Float> SCALE_Y
         // A Property wrapper around the scaleY functionality handled by the setScaleY(float) and getScaleY() methods.
